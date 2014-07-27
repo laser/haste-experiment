0a87690b415a43061e59dd6fa4a59349a8b0a470
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-07-27 15:29:04 -0700

    initial client state, enter-key submit

--- server-todos/todo-server.hs
+++ server-todos/todo-server.hs
@@ -1,5 +1,4 @@
 import Haste.App
-import qualified Haste.App.Concurrent as H
 import qualified Control.Concurrent as C
 
 type Todo = String
@@ -15,29 +14,61 @@ buildUI root = do
   setChildren root [ul, input, saveButton]
   return TodoUI { todoList=ul, input=input, saveButton=saveButton }
 
-addTodo :: MonadIO m => m (C.MVar [Todo]) -> Todo -> m [Todo]
-addTodo container todo = do
-  current <- container
-  liftIO $ do
-    todos <- C.takeMVar current
-    C.putMVar current $ todo : todos
-    C.readMVar current
+buildTodoEls :: MonadIO m => TodoUI -> [String] -> m ()
+buildTodoEls ui todos = do
+  els <- mapM createTodoEl todos
+  setChildren (todoList ui) els
+  where
+    createTodoEl text = do
+      el  <- newElem "li"
+      txt <- newTextElem text
+      addChild txt el
+      return el
 
-createTodoEl :: MonadIO m => String -> m Elem
-createTodoEl text = do
-  el <- newElem "li"
-  txt <- newTextElem text
-  addChild txt el
-  return el
+takeValue :: MonadIO m => Elem -> m String
+takeValue el = do
+  new <- getProp el "value"
+  setProp el "value" ""
+  return new
 
 main :: IO ()
 main = do
   runApp (mkConfig "ws://localhost:24601" 24601) $ do
-    todos <- liftServerIO $ C.newMVar $ ([] :: TodoList)
-    addTodo <- remote $ (addTodo todos)
-    runClient $ withElem "root" buildUI >>= \ui ->
+
+    -- initialize the list of todos on the server
+    todos <- liftServerIO $ C.newMVar ([] :: TodoList)
+
+    -- make our todo-adding function available to client
+    -- as an API call
+    addTodo <- remote $ \todo -> do
+      mv <- todos
+      liftIO $ do
+        todos <- C.takeMVar mv
+        C.putMVar mv $ todos ++ [todo]
+        C.readMVar mv
+
+    -- need to expose a way to get the todos on first load
+    -- too
+    getTodos <- remote $ todos >>= liftIO . C.readMVar
+
+    -- client application
+    runClient $ withElem "root" buildUI >>= \ui -> do
+
+      -- create todo elements for everything in server's
+      -- cache o' todos
+      onServer getTodos >>= buildTodoEls ui
+
+      -- event handler to add new todo to server's list
+      (input ui) `onEvent` OnKeyUp $ \keycode -> case keycode of
+        13 -> do
+          newTodo <- takeValue (input ui)
+          updated <- onServer $ addTodo <.> newTodo
+          buildTodoEls ui updated
+        _ -> return ()
+
+      -- same thing, but for mouse cause i can't figure out
+      -- how to share the same event-handler
       (saveButton ui) `onEvent` OnClick $ \_ _ -> do
-        newTodo <- getProp (input ui) "value"
+        newTodo <- takeValue (input ui)
         updated <- onServer $ addTodo <.> newTodo
-        els     <- mapM createTodoEl $ reverse updated
-        setChildren (todoList ui) els
+        buildTodoEls ui updated
