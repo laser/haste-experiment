84c4b596a70b7dc8215b5209c93fbabf6c533cb8
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-07-29 09:34:34 -0700

    cleaning up type aliases, exporting UI differently, etc.

--- server-todos/todo-server.hs
+++ server-todos/todo-server.hs
@@ -1,12 +1,52 @@
+import Control.Applicative
 import Haste.App
 import qualified Control.Concurrent as C
 
-type Todo = String
+type Todo     = String
 type TodoList = [Todo]
 
-data TodoUI = TodoUI { todoList :: Elem, input:: Elem, saveButton :: Elem }
+-- our application state consists of MVar containing
+-- a list of Todo-values
+type AppState = C.MVar TodoList
 
-buildUI :: MonadIO m => Elem -> m TodoUI
+data TodoUI = TodoUI {
+  todoList   :: Elem,
+  input      :: Elem,
+  saveButton :: Elem
+}
+
+data TodoAPI = TodoAPI {
+  apiAddTodo  :: Remote (Todo -> Server TodoList),
+  apiGetTodos :: Remote (Server TodoList)
+}
+
+appendTodo :: Server (AppState) -> Todo -> Server TodoList
+appendTodo state todo = do
+  mv <- state
+  liftIO $ do
+    todos <- C.takeMVar mv
+    C.putMVar mv $ todos ++ [todo]
+    C.readMVar mv
+
+syncTodos :: TodoUI -> Remote (Todo -> Server TodoList) -> Client ()
+syncTodos ui addTodo = do
+  newTodo <- takeVal $ input ui
+  updated <- onServer $ addTodo <.> newTodo
+  syncTodoUI ui updated
+
+syncTodoUI :: TodoUI -> TodoList -> Client ()
+syncTodoUI ui todos = do
+  els <- mapM createTodoEl todos
+  setChildren (todoList ui) els
+  where
+    createTodoEl :: String -> Client Elem
+    createTodoEl text = do
+      txt <- newTextElem text
+      el  <- newElem "li"
+      addChild txt el
+      return el
+
+buildUI :: Elem -> Client TodoUI
 buildUI root = do
   [ul, input, saveButton] <- mapM newElem ["ul", "input", "button"]
   saveTxt <- newTextElem "save"
@@ -14,22 +54,20 @@ buildUI root = do
   setChildren root [ul, input, saveButton]
   return TodoUI { todoList=ul, input=input, saveButton=saveButton }
 
-buildTodoEls :: MonadIO m => TodoUI -> [String] -> m ()
-buildTodoEls ui todos = do
-  els <- mapM createTodoEl todos
-  setChildren (todoList ui) els
-  where
-    createTodoEl text = do
-      el  <- newElem "li"
-      txt <- newTextElem text
-      addChild txt el
-      return el
+swapVal :: String -> Elem -> Client String
+swapVal new el = do
+  old <- getProp el "value"
+  setProp el "value" new
+  return old
 
-takeValue :: MonadIO m => Elem -> m String
-takeValue el = do
-  new <- getProp el "value"
-  setProp el "value" ""
-  return new
+takeVal :: Elem -> Client String
+takeVal = swapVal ""
+
+hole1 :: a -> (b -> a)
+hole1 fx = \_ -> fx
+
+hole2 :: a -> (c -> b -> a)
+hole2 fx = \_ -> hole1 fx
 
 main :: IO ()
 main = do
@@ -38,37 +76,27 @@ main = do
     -- initialize the list of todos on the server
     todos <- liftServerIO $ C.newMVar ([] :: TodoList)
 
-    -- make our todo-adding function available to client
-    -- as an API call
-    addTodo <- remote $ \todo -> do
-      mv <- todos
-      liftIO $ do
-        todos <- C.takeMVar mv
-        C.putMVar mv $ todos ++ [todo]
-        C.readMVar mv
+    -- make our todo-adding functions available to client
+    -- as API calls
+    f1 <- remote (appendTodo todos)
+    f2 <- remote (todos >>= liftIO . C.readMVar)
+    let api = TodoAPI f1 f2
 
-    -- need to expose a way to get the todos on first load
-    -- too
-    getTodos <- remote $ todos >>= liftIO . C.readMVar
+    -- TODO: how the heck does this work??
+    {-api <- TodoAPI <$> remote (appendTodo todos)-}
+                   {-<*> remote (todos >>= liftIO . C.readMVar)-}
 
     -- client application
     runClient $ withElem "root" buildUI >>= \ui -> do
 
       -- create todo elements for everything in server's
       -- cache o' todos
-      onServer getTodos >>= buildTodoEls ui
+      onServer (apiGetTodos api) >>= syncTodoUI ui
 
       -- event handler to add new todo to server's list
       (input ui) `onEvent` OnKeyUp $ \keycode -> case keycode of
-        13 -> do
-          newTodo <- takeValue (input ui)
-          updated <- onServer $ addTodo <.> newTodo
-          buildTodoEls ui updated
-        _ -> return ()
+        13 -> syncTodos ui $ apiAddTodo api
+        _  -> return ()
 
-      -- same thing, but for mouse cause i can't figure out
-      -- how to share the same event-handler
-      (saveButton ui) `onEvent` OnClick $ \_ _ -> do
-        newTodo <- takeValue (input ui)
-        updated <- onServer $ addTodo <.> newTodo
-        buildTodoEls ui updated
+      -- same thing, but for mouse
+      (saveButton ui) `onEvent` OnClick $ hole2 $ syncTodos ui $ apiAddTodo api
