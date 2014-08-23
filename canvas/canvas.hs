4029e8e8351a47f9b6545ff8c4fee92d6a99f2da
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-08-23 13:18:18 -0700

    reformat / simplication of canvas example

--- canvas/canvas.hs
+++ canvas/canvas.hs
@@ -3,19 +3,19 @@ module Main where
 import Haste
 import Haste.Graphics.Canvas
 
-square = do
-  rect (-40, -40) (40, 40)
-  rect (-10, -10) (10, 10)
-
 main = do
   Just canvas <- getCanvasById "canvas"
   animate canvas 0
 
+square = do
+  rect (-40, -40) (40, 40)
+  rect (-10, -10) (10, 10)
+
 animate canvas angle = do
-  let stroked = stroke square
-  let rotated = rotate angle stroked
-  let drawn   = translate (70, 70) rotated
+  render canvas
+    $ translate (70, 70)
+    $ rotate angle
+    $ stroke square
 
-  render canvas drawn
-
-  setTimeout 10 $ animate canvas (angle + 0.01)
+  setTimeout 10
+    $ animate canvas (angle + 0.01)
