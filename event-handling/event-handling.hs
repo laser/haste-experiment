ac7b923dcea5146dbcf1aff9feb9c858126b0591
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-08-25 15:13:54 -0700

    a little golfing of event-handler

--- event-handling/event-handling.hs
+++ event-handling/event-handling.hs
@@ -3,8 +3,9 @@ module Main where
 import Haste
 
 main = withElem "cat" $ \img -> do
-  onEvent img OnMouseOver $ \_ -> do
-    setAttr img "style" "border: 10px solid;"
+  onEvent img OnMouseOver
+    $ const
+    $ setAttr img "style" "border: 10px solid;"
 
-  onEvent img OnMouseOut $ do
-    setAttr img "style" "border: none;"
+  onEvent img OnMouseOut
+    $ setAttr img "style" "border: none;"
