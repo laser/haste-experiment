3aa54aae1787a2f181d3bd6481b370e67c355278
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-08-25 18:09:20 -0700

    cleaning up ajax example

--- ajax/ajax.hs
+++ ajax/ajax.hs
@@ -3,13 +3,10 @@ module Main where
 import Haste
 import Haste.Ajax
 
-main = withElems ["btn", "out"] app
-
-app [btn, out] = do
-  onEvent btn OnClick $ \_ _ -> do
-    textRequest GET "/api" [] $ \mstr ->
-      case mstr of
-        Just s  -> txt out s
-        Nothing -> txt out ""
-
-txt el s = setProp el "innerHTML" s
+main = withElems ["btn", "out"]
+  $ \[btn, out] -> onEvent btn OnClick
+    $ \_ _ -> textRequest GET "/api" []
+      $ \res ->
+        case res of
+          Just s  -> setProp out "innerHTML" s
+          Nothing -> setProp out "innerHTML" ""
