ecf60e26cd3a4943738def575e5f35b77e0344bf
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-08-26 17:12:57 -0700

    fixing routing demo + styles

--- demos/routing/routing.hs
+++ demos/routing/routing.hs
@@ -2,13 +2,13 @@ module Main where
 
 import Haste
 
-main = withElem "message" 
+main = withElem "message"
   $ \el -> do
     hash <- getHash
     let msg = if null hash then "hello"
               else hash
-    setProp el "innerHTML" msg
+    setProp el "value" msg
 
-    onHashChange 
+    onHashChange
       $ \_ msg ->
-        setProp el "innerHTML" msg
+        setProp el "value" msg
