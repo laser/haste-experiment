6b79adbe676d3e2ed195628233435b277034d034
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-08-26 20:36:44 -0700

    adding fast-scroll amount for demo

--- demos/dom/dom.hs
+++ demos/dom/dom.hs
@@ -2,12 +2,13 @@ module Main where
 
 import Haste
 
-main = withElem "root" 
+main = withElem "root"
   $ \root -> do
     marq <- newElem "marquee"
     txt  <- newTextElem "Mark"
 
     setAttr marq "id" "banner"
+    setAttr marq "scrollamount" "20"
 
     addChild txt marq
     addChild marq root
