905588a6f97e81dffb7feb4cce8e4e872d170b2e
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-04 18:33:37 -0700

    updating Makefile + client-server app names

--- demos/client-server/client-server.hs
+++ demos/client-server/client-server.hs
@@ -5,7 +5,7 @@ main = runApp (mkConfig "ws://0.0.0.0:24601" 24601) $ do
   counter <- liftServerIO $ newIORef (0 :: Int)
   inc'    <- remote $ increment counter
 
-  runClient $ withElems ["btn", "out"] $ \[btn, out] ->
+  runClient $ withElems ["btn", "out"] $ \[btn, out] -> do
     onEvent btn OnClick $ \_ _ -> do
       n <- onServer inc'
       setProp out "innerHTML" $ show n
