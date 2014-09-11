d0a374118cbaadd6f1a0838fd3b03e19d4382cd2
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-11 08:14:43 -0700

    simplifying client-server example

--- demos/client-server/client-server.hs
+++ demos/client-server/client-server.hs
@@ -1,16 +1,15 @@
 import Haste.App
 import Data.IORef
 
-main = runApp (mkConfig "ws://0.0.0.0:24601" 24601) $ do
-  counter <- liftServerIO $ newIORef (0 :: Int)
-  inc'    <- remote $ increment counter
+main :: IO ()
+main = runApp (mkConfig "ws://0.0.0.0:24601" 24601) clientMain
 
-  runClient $ withElems ["btn", "out"] $ \[btn, out] -> do
-    onEvent btn OnClick $ \_ _ -> do
-      n <- onServer inc'
-      setProp out "innerHTML" $ show n
+echoServer :: String -> Server ()
+echoServer s = liftIO . putStrLn $ "The fox said: " ++ s
 
-increment :: Server (IORef Int) -> Server Int
-increment serverState = do
-  ref <- serverState
-  liftIO $ atomicModifyIORef ref $ \x -> (x + 1, x)
+clientMain :: App Done
+clientMain = do
+  tellServer <- remote $ echoServer
+  runClient $ do
+    name <- prompt "What does the fox say?"
+    onServer $ tellServer <.> name
